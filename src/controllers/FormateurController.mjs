af4ef0758e6a766887de38b5bf7bc04daad67a09
ghollamsimo <ghollamsimo1@gmail.com>
2024-09-09 11:36:57 +0100

    Adding The Files in the each directory

---
+++ src/controllers/FormateurController.mjs
@@ -0,0 +1,5 @@
+import FormateurModel from '../models/FormateurModel.mjs'
+
+class FormateurController{
+    //put your own code here
+}
---
+++ src/models/FormateurModel.mjs
@@ -0,0 +1,5 @@
+import {db} from '../config/settings.mjs'
+
+class FormateurModel{
+    //put you own code here
+}
