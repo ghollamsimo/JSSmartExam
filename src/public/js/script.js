76e888146d07b9c30e8ee953b153c9e1d85d0201
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2024-09-12 17:16:35 +0100

    Votre message de commit

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -17,3 +17,13 @@ function convertToJSON() {
 
     checkboxes.forEach(cb => cb.remove());
 }
+
+
+
+    const hamburger = document.querySelector('.hamburger');
+    const navLink = document.querySelector('.nav__link');
+
+    hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+    });
+  
