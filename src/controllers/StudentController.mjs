d840b8d6936a596b04fd72410667183e6b0644c8
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2024-09-15 16:53:16 +0100

    modiffication sur email

--- src/controllers/StudentController.mjs
+++ src/controllers/StudentController.mjs
@@ -11,7 +11,6 @@ class StudentController{
         })
     }
 
-     // Insert a new student
      static async createStudent(req, res) {
         const { nom, prenom, date_naissance, adresse, date_inscription,email } = req.body;
 
@@ -24,28 +23,41 @@ class StudentController{
             email
         };
 
-        // Insertion dans la base de données
         StudentModel.add(newStudent, async (err, result) => {
             if (err) {
                 return res.status(500).json({ message: "Error adding student" });
             }
 
-            // Si l'insertion est réussie, envoie un email à l'étudiant
             try {
                 await EmailService.sendEmail({
                     to: email,
                     subject: "Bienvenue à l'école",
                     text: `Bonjour ${prenom}, bienvenue dans notre école !`,
-                    html: `<h1>Bonjour ${prenom} ${nom}</h1>
-                           <p>Bienvenue à notre école ! Veuillez trouver votre email adresse  : <strong>${email}</strong>.</p>
-                           <p>Cordialement,<br>L'équipe de l'école</p>`
+                    html: `<div style="font-family: Arial, sans-serif; color: #333;">
+                <div style="text-align: center;">
+                    <img src="http://localhost:3000/assets/logo.png" alt="Logo" style="width: 150px; margin-bottom: 20px;">
+                </div>
+                <h1 style="color: #4CAF50;">Bonjour ${prenom} ${nom}</h1>
+                <p style="font-size: 16px;">
+                    Bienvenue dans notre école ! Veuillez utiliser votre email pour accéder à notre plateforme.
+                    </br>
+                    <h3>Veuillez trouver votre email adresse  : <strong>${email}</strong>.</p></h3>
+                </p>
+                <p style="font-size: 14px; color: #777;">
+                    Nous sommes ravis de vous accueillir et nous vous souhaitons une excellente expérience.
+                </p>
+                <footer style="margin-top: 30px; font-size: 12px; color: #888;">
+                    <p>École XYZ, Adresse, Ville</p>
+                </footer>
+            </div>
+            `
                 });
                 console.log(`Email envoyé à ${email}`);
             } catch (emailErr) {
                 console.error("Erreur lors de l'envoi de l'email : ", emailErr);
             }
 
-            // Redirection après succès
+            
             res.redirect('/');
         });
     }
